b26555497a8d8d0d38531ed613b928569293b041
myat-su-mon <myatsumon@ucsy.edu.mm>
2021-01-19 20:49:42 +0630

    swd finished

---
+++ 3_javascript/data-types.js
@@ -0,0 +1,19 @@
+// String assignment
+let first = "james";
+let name = 'thomas';
+let fakeName = `tom`;
+
+    let x = 15;
+    let name = "John William";
+    let age = 22;
+
+    let arr = ["apple", "orange", "mango", "banana"];
+
+    let myself = {
+        name: "Jessica",
+        age: 22, 
+        job: "developer"
+    };
+
+    let z = x + 15;
+    z++;
